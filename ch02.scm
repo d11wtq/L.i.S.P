66214c916127936ba595dccb523565c9c75fea2a
d11wtq <chris@w3style.co.uk>
2014-03-28 12:42:44 +0000

    The Lisp2 from Chapter 2 is now a Lisp3

--- ch02.scm
+++ ch02.scm
@@ -1,14 +1,15 @@
 ;;;;
 ;;; Lisp in Small Pieces, Chapter 2.
 ;;;
-;;; A very naive direct-execution Lisp2 interpreter.
+;;; A very naive direct-execution Lisp3 interpreter.
 ;;;;
 
 (define *env.init*   (list))
 (define *env.global* *env.init*)
 (define *fenv.global* *env.init*)
+(define *denv.global* *env.init*)
 
-(define (evaluate sexp env fenv)
+(define (evaluate sexp env fenv denv)
   "Defines the core evaluator of this LiSP."
   (if (atom? sexp)
     (if (symbol? sexp)
@@ -16,13 +17,13 @@
       sexp)
     (case (car sexp)
       ((quote) (cadr sexp))
-      ((if) (if (evaluate (cadr sexp) env fenv)
-              (evaluate (caddr sexp) env fenv)
-              (evaluate (cadddr sexp) env fenv)))
-      ((begin) (eprogn (cdr sexp) env fenv))
+      ((if) (if (evaluate (cadr sexp) env fenv denv)
+              (evaluate (caddr sexp) env fenv denv)
+              (evaluate (cadddr sexp) env fenv denv)))
+      ((begin) (eprogn (cdr sexp) env fenv denv))
       ((set!) (update! (cadr sexp)
                        env
-                       (evaluate (caddr sexp) env fenv)))
+                       (evaluate (caddr sexp) env fenv denv)))
       ((flet) (eprogn (cddr sexp)
                       env
                       (extend fenv
@@ -32,17 +33,32 @@
                                                     (cddr defn)
                                                     env
                                                     fenv))
-                                   (cadr sexp)))))
+                                   (cadr sexp)))
+                      denv))
       ((function) (cond
                     ((symbol? (cadr sexp)) (lookup (cadr sexp) fenv))
                     (else (error "Bad function" (cadr sexp)))))
+      ((dynamic) (lookup (cadr sexp) denv))
+      ((dynamic-set!) (update! (cadr sexp)
+                               denv
+                               (evaluate (caddr sexp) env fenv denv)))
+      ((dynamic-let) (eprogn (cddr sexp)
+                             env
+                             fenv
+                             (extend denv
+                                     (map car (cadr sexp))
+                                     (map (lambda (defn)
+                                            (evaluate defn env fenv denv))
+                                          (map cadr (cadr sexp))))))
       ((lambda) (make-function (cadr sexp)
                                (cddr sexp)
-                               env fenv))
+                               env
+                               fenv))
       (else (evaluate-application (car sexp)
-                                  (evlis (cdr sexp) env fenv)
+                                  (evlis (cdr sexp) env fenv denv)
                                   env
-                                  fenv)))))
+                                  fenv
+                                  denv)))))
 
 (define (atom? sexp)
   "Return #t if the sexp is not a pair."
@@ -66,45 +82,47 @@
       (update! id (cdr env) value))
     (error "No such binding" id)))
 
-(define (eprogn sexps env fenv)
+(define (eprogn sexps env fenv denv)
   "Evaluate a sequence of sexps and return the last one."
   (if (pair? sexps)
     (if (pair? (cdr sexps))
       (begin
-        (evaluate (car sexps) env fenv)
-        (eprogn (cdr sexps) env fenv))
-      (evaluate (car sexps) env fenv))
+        (evaluate (car sexps) env fenv denv)
+        (eprogn (cdr sexps) env fenv denv))
+      (evaluate (car sexps) env fenv denv))
     (list)))
 
-(define (evlis sexps env fenv)
+(define (evlis sexps env fenv denv)
   "Evaluate a list of sexps and return the evaluated list."
   (if (pair? sexps)
-    (cons (evaluate (car sexps) env fenv)
-          (evlis (cdr sexps) env fenv))
+    (cons (evaluate (car sexps) env fenv denv)
+          (evlis (cdr sexps) env fenv denv))
     (list)))
 
 (define (make-function params body env fenv)
   "Create a function with params and body in env."
-  (lambda (values)
+  (lambda (values denv)
     (eprogn body
             (extend env params values)
-            fenv)))
+            fenv
+            denv)))
 
-(define (evaluate-application id args env fenv)
+(define (evaluate-application id args env fenv denv)
   "Evaluate a function call to id with args in env."
   (cond
-    ((symbol? id) (invoke (lookup id fenv) args))
+    ((symbol? id) (invoke (lookup id fenv) args denv))
     ((and (pair? id) (eq? (car id) 'lambda)) (eprogn (cddr id)
                                                      (extend env
                                                              (cadr id)
                                                              args)
-                                                     fenv))
+                                                     fenv
+                                                     denv))
     (else (error "Bad functional term" id))))
 
-(define (invoke f args)
+(define (invoke f args denv)
   "Call a function in the interpreter."
   (if (procedure? f)
-    (f args)
+    (f args denv)
     (error "Cannot apply value:" f)))
 
 (define (extend env ids values)
@@ -164,7 +182,10 @@
 (define (run-repl)
   "Run a simple repl session for our interpreter."
   (define (repl)
-    (let ((value (evaluate (read) *env.global* *fenv.global*)))
+    (let ((value (evaluate (read)
+                           *env.global*
+                           *fenv.global*
+                           *denv.global*)))
       (if (eof-object? value)
         (newline)
         (begin
