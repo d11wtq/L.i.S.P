fe38d5a1010f2adc2816b34584e18f0f08335e44
d11wtq <chris@w3style.co.uk>
2014-03-29 09:43:18 +0000

    Lisp3 from Chapter 2 is a Lisp2 again.

--- ch02.scm
+++ ch02.scm
@@ -1,15 +1,13 @@
 ;;;;
 ;;; Lisp in Small Pieces, Chapter 2.
 ;;;
-;;; A very naive direct-execution Lisp3 interpreter.
+;;; A very naive direct-execution Lisp2 interpreter.
 ;;;;
 
 (define *env.init*   (list))
 (define *env.global* *env.init*)
-(define *fenv.global* *env.init*)
-(define *denv.global* *env.init*)
 
-(define (evaluate sexp env fenv denv)
+(define (evaluate sexp env denv)
   "Defines the core evaluator of this LiSP."
   (if (atom? sexp)
     (if (symbol? sexp)
@@ -17,60 +15,39 @@
       sexp)
     (case (car sexp)
       ((quote) (cadr sexp))
-      ((if) (if (evaluate (cadr sexp) env fenv denv)
-              (evaluate (caddr sexp) env fenv denv)
-              (evaluate (cadddr sexp) env fenv denv)))
-      ((begin) (eprogn (cdr sexp) env fenv denv))
+      ((if) (if (evaluate (cadr sexp) env denv)
+              (evaluate (caddr sexp) env denv)
+              (evaluate (cadddr sexp) env denv)))
+      ((begin) (eprogn (cdr sexp) env denv))
       ((set!) (update! (cadr sexp)
                        env
-                       (evaluate (caddr sexp) env fenv denv)))
-      ((flet) (eprogn (cddr sexp)
-                      env
-                      (extend fenv
-                              (map car (cadr sexp))
-                              (map (lambda (defn)
-                                     (make-function (cadr defn)
-                                                    (cddr defn)
-                                                    env
-                                                    fenv))
-                                   (cadr sexp)))
-                      denv))
-      ((function) (cond
-                    ((symbol? (cadr sexp)) (lookup (cadr sexp) fenv))
-                    (else (error "Bad function" (cadr sexp)))))
-      ((dynamic) (lookup (cadr sexp) denv))
-      ((dynamic-set!) (update! (cadr sexp)
-                               denv
-                               (evaluate (caddr sexp) env fenv denv)))
-      ((dynamic-let) (eprogn (cddr sexp)
-                             env
-                             fenv
-                             (extend denv
-                                     (map car (cadr sexp))
-                                     (map (lambda (defn)
-                                            (evaluate defn env fenv denv))
-                                          (map cadr (cadr sexp))))))
+                       (evaluate (caddr sexp) env denv)))
       ((lambda) (make-function (cadr sexp)
                                (cddr sexp)
-                               env
-                               fenv))
-      (else (evaluate-application (car sexp)
-                                  (evlis (cdr sexp) env fenv denv)
-                                  env
-                                  fenv
-                                  denv)))))
+                               env))
+      (else (invoke (evaluate (car sexp) env denv)
+                    (evlis (cdr sexp) env denv)
+                    denv)))))
 
 (define (atom? sexp)
   "Return #t if the sexp is not a pair."
   (not (pair? sexp)))
 
-(define (lookup id env)
-  "Find the value of id in the a-list env."
+(define (lookup-with-error-function id env error-fn)
+  "Find the value of id in the a-list env, otherwise invoke error-fn."
   (if (pair? env)
     (if (eq? (caar env) id)
       (cdar env)
-      (lookup id (cdr env)))
-    (error "No such binding" id)))
+      (lookup-with-error-function id (cdr env) error-fn))
+    (error-fn id)))
+
+(define (lookup id env)
+  "Find the value of id in the a-list env, otherwise error."
+  (lookup-with-error-function
+    id
+    env
+    (lambda (args denv)
+      (error "No such binding" (car args)))))
 
 (define (update! id env value)
   "Change the value stored in the binding id in the a-list env."
@@ -82,48 +59,22 @@
       (update! id (cdr env) value))
     (error "No such binding" id)))
 
-(define (eprogn sexps env fenv denv)
+(define (eprogn sexps env denv)
   "Evaluate a sequence of sexps and return the last one."
   (if (pair? sexps)
     (if (pair? (cdr sexps))
       (begin
-        (evaluate (car sexps) env fenv denv)
-        (eprogn (cdr sexps) env fenv denv))
-      (evaluate (car sexps) env fenv denv))
+        (evaluate (car sexps) en denvv)
+        (eprogn (cdr sexps) env denv))
+      (evaluate (car sexps) env denv))
     (list)))
 
-(define (evlis sexps env fenv denv)
+(define (evlis sexps env denv)
   "Evaluate a list of sexps and return the evaluated list."
   (if (pair? sexps)
-    (cons (evaluate (car sexps) env fenv denv)
-          (evlis (cdr sexps) env fenv denv))
+    (cons (evaluate (car sexps) env denv)
+          (evlis (cdr sexps) env denv))
     (list)))
-
-(define (make-function params body env fenv)
-  "Create a function with params and body in env."
-  (lambda (values denv)
-    (eprogn body
-            (extend env params values)
-            fenv
-            denv)))
-
-(define (evaluate-application id args env fenv denv)
-  "Evaluate a function call to id with args in env."
-  (cond
-    ((symbol? id) (invoke (lookup id fenv) args denv))
-    ((and (pair? id) (eq? (car id) 'lambda)) (eprogn (cddr id)
-                                                     (extend env
-                                                             (cadr id)
-                                                             args)
-                                                     fenv
-                                                     denv))
-    (else (error "Bad functional term" id))))
-
-(define (invoke f args denv)
-  "Call a function in the interpreter."
-  (if (procedure? f)
-    (f args denv)
-    (error "Cannot apply value:" f)))
 
 (define (extend env ids values)
   "Extend the environment env with new ids and values."
@@ -138,6 +89,38 @@
                    (error "Too many values")))
     ((symbol? ids) (cons (cons ids values)
                          env))))
+
+(define (make-function params body env)
+  "Create a function with params and body in env."
+  (lambda (values denv)
+    (eprogn body
+            (extend env params values)
+            denv)))
+
+(define (invoke f args denv)
+  "Call a function in the interpreter."
+  (if (procedure? f)
+    (f args denv)
+    (error "Cannot apply value:" f)))
+
+(define (run-repl)
+  "Run a simple repl session for our interpreter."
+  (define (repl)
+    (let ((value (evaluate (read)
+                           *env.global*
+                           *env.init*)))
+      (if (eof-object? value)
+        (newline)
+        (begin
+          (display value)
+          (newline)
+          (repl)))))
+  (display "Entering interactive ch01 repl.")
+  (newline)
+  (display "To exit, type (quit) or hit ctrl-d.")
+  (newline)
+  (newline)
+  (repl))
 
 (define-syntax definitial
   (syntax-rules ()
@@ -154,56 +137,42 @@
          (cons (cons 'name value) *env.global*))
        'name))))
 
-(define-syntax definitial-function
-  (syntax-rules ()
-    "Macro to define an initial function in the global env."
-    ((definitial-function name)
-     (begin
-       (set! *fenv.global*
-         (cons (cons 'name 'void)
-               *fenv.global*))
-       'name))
-    ((definitial-function name value)
-     (begin
-       (set! *fenv.global*
-         (cons (cons 'name value) *fenv.global*))
-       'name))))
-
 (define-syntax defprimitive
   (syntax-rules ()
     "Macro to bind a primitive scheme function to the global env."
     ((defprimitive name fn arity)
-     (definitial-function name
-       (lambda (values)
+     (definitial name
+       (lambda (values denv)
          (if (= arity (length values))
            (apply fn values)
            (error "Bad arity" (list 'name values))))))))
-
-(define (run-repl)
-  "Run a simple repl session for our interpreter."
-  (define (repl)
-    (let ((value (evaluate (read)
-                           *env.global*
-                           *fenv.global*
-                           *denv.global*)))
-      (if (eof-object? value)
-        (newline)
-        (begin
-          (display value)
-          (newline)
-          (repl)))))
-  (display "Entering interactive ch02 repl.")
-  (newline)
-  (display "To exit, type (quit) or hit ctrl-d.")
-  (newline)
-  (newline)
-  (repl))
 
 (define (quit)
   "Provides a function to exit the repl."
   (read (open-input-string "")))
 
-;;; Bootstrap
+(definitial bind/de
+            (lambda (args denv)
+              (if (= 3 (length args))
+                (let ((id (car args))
+                      (value (cadr args))
+                      (thunk (caddr args)))
+                  (invoke thunk
+                          (list)
+                          (extend denv
+                                  (list id)
+                                  (list value))))
+                (error "Bad arity" 'bind/de))))
+
+(definitial assoc/de
+            (lambda (args denv)
+              (if (= 2 (length args))
+                (let ((id (car args))
+                      (default (cadr args)))
+                  (lookup-with-error-function id denv default))
+                (error "Bad arity" 'assoc/de))))
+
+;;; Primitives
 
 (definitial t #t)
 (definitial f #f)
@@ -222,9 +191,3 @@
 (defprimitive > > 2)
 (defprimitive = = 2)
 (defprimitive quit quit 0)
-
-(definitial-function funcall
-                     (lambda (args)
-                      (if (> (length args) 1)
-                        (invoke (car args) (cdr args))
-                        (error "Bad arity" 'funcall))))
